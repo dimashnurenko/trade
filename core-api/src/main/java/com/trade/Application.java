b614bf8bfcb96fa0bdf5d7dfbd362cd27cdae19e
dimas <dimashnurenko@gmail.com>
2018-08-20 22:58:17 +0300

    cloud init

--- core-api/src/main/java/com/trade/Application.java
+++ core-api/src/main/java/com/trade/Application.java
@@ -2,6 +2,7 @@ package com.trade;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -14,24 +15,25 @@ import static java.util.TimeZone.setDefault;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
-			}
-		};
-	}
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 
-	@PostConstruct
-	void setUTCTimezone() {
-		setDefault(getTimeZone("UTC"));
-	}
+    @PostConstruct
+    void setUTCTimezone() {
+        setDefault(getTimeZone("UTC"));
+    }
 }
